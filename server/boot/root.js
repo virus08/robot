099ba8c6b07bddef2a4e7992d1c0aa8ef6545b4a
virus08 <virus08@hotmail.com>
2018-09-04 11:09:55 +0700

    test

--- server/boot/root.js
+++ server/boot/root.js
@@ -3,6 +3,12 @@
 module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.use('/express-status', function(req, res, next) {
+    res.json({ running: true });
+  });
+  router.get('/ping', function(req, res) {
+    res.send('pongaroo');
+  });
   server.use(router);
 };
+
